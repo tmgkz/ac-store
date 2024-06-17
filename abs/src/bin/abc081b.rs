77798543523c84f4ac05a9850298ce6b1ee9aec4
tmgkz <tumugikaze@gmail.com>
2024-06-17 23:14:16 +0900

    ABC081B

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut s: [usize;n]
+    }
+    let mut cnt = 0;
+
+    loop {
+        if s.iter().all(|&x| x % 2 == 0) {
+            s = s.iter().map(|x| x / 2).collect();
+            cnt += 1;
+        } else {
+            break;
+        }
+    }
+
+    println!("{:?}", cnt)
 }
