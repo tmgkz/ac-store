74811b2aabf6a8c8fe8fc5783b4f5a39514392a6
tmgkz <tumugikaze@gmail.com>
2024-06-17 21:47:26 +0900

    filterを使用した解法

--- abs/src/bin/abc081a.rs
+++ abs/src/bin/abc081a.rs
@@ -4,15 +4,17 @@ fn main() {
     input! {
         s: String,
     }
-    let arr = s.chars().collect::<Vec<char>>();
-    let mut count = 0;
+    // let arr = s.chars().collect::<Vec<char>>();
+    // let mut count = 0;
+    //
+    // for number in arr {
+    //     match number {
+    //         '1' => count += 1,
+    //         _ => (),
+    //     }
+    // }
 
-    for number in arr {
-        match number {
-            '1' => count += 1,
-            _ => (),
-        }
-    }
+    let count = s.chars().filter(|&x| x == '1').count();
 
     println!("{}", count)
 }
