42f4571c916a394b2553a2195066ceb51bc88365
tmgkz <tumugikaze@gmail.com>
2024-06-20 00:20:23 +0900

    ABC083B 途中

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        a: i32,
+        b: i32
+    }
+    let mut numbers = Vec::<i32>::new();
+    for c in 1..n + 1 {
+        let s = c.chars().sum();
+        if a <= s && s <= b {
+            numbers.push(c)
+        }
+    }
+    println!("{}", numbers.iter().sum())
 }
