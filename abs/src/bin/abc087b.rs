db50c5874529553fc48e679575b58a565d192ca5
tmgkz <tumugikaze@gmail.com>
2024-06-19 23:40:44 +0900

    ABC087B

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        x: i32
+    }
+    let mut cnt: i32 = 0;
+    for i in 0..a + 1 {
+        for j in 0..b + 1 {
+            for k in 0..c + 1 {
+                if 500 * i + 100 * j + 50 * k == x {
+                    cnt += 1
+                }
+            }
+        }
+    }
+    println!("{}", cnt)
 }
