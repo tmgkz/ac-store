2aebd8e1ee2c8143f5124d2dc403acd105e79937
tmgkz <tumugikaze@gmail.com>
2024-06-25 22:56:26 +0900

    abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -6,12 +6,29 @@ fn main() {
         a: i32,
         b: i32
     }
-    let mut numbers = Vec::<i32>::new();
-    for c in 1..n + 1 {
-        let s = c.chars().sum();
-        if a <= s && s <= b {
-            numbers.push(c)
-        }
+    let numbers: Vec<i32> = (1..n + 1).collect();
+    println!(
+        "{}",
+        numbers
+            .iter()
+            .filter(|x| is_range(sum_single_number(x), a, b))
+            .sum::<i32>(),
+    )
+}
+
+fn is_range(x: i32, a: i32, b: i32) -> bool {
+    a <= x && x <= b
+}
+
+fn sum_single_number(i: &i32) -> i32 {
+    let mut result = Vec::new();
+    let mut number: i32 = *i;
+    if number.to_string().len() == 1 {
+        return number;
     }
-    println!("{}", numbers.iter().sum())
+    for _ in 1..number.to_string().len() + 1 {
+        result.push(number % 10);
+        number = number / 10;
+    }
+    result.iter().sum()
 }
