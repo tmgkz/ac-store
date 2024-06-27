6f2a089cb302a76bc39d4ec0db00f5a4d1000c4d
tmgkz <tumugikaze@gmail.com>
2024-06-28 00:07:08 +0900

    abc085c

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        y: i32
+    }
+    for i in 0..=n {
+        for j in 0..=n - i {
+            let k = n - i - j;
+            if i * 10000 + j * 5000 + k * 1000 == y {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1")
 }
