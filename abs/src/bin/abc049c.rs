9e6616e88400e68f53cc740f07d7a41283baa98b
tmgkz <tumugikaze@gmail.com>
2024-07-01 20:35:38 +0900

    ABC049C

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,44 @@
+use proconio::input;
+use std::collections::VecDeque;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+    }
+    if s.len() <= 4 {
+        println!("NO");
+        return;
+    }
+    let mut str: VecDeque<char> = s.chars().rev().collect();
+    loop {
+        if str.len() == 0 {
+            println!("YES");
+            return;
+        }
+        let a = str.drain(0..5).collect::<VecDeque<_>>();
+        let mut joined_str = join_str(&a);
+        if joined_str == "maerd" || joined_str == "esare" {
+            continue;
+        }
+        let a = str.drain(0..1).collect::<VecDeque<_>>();
+        joined_str.push_str(join_str(&a).as_str());
+        if joined_str == "resare" {
+            continue;
+        }
+        let a = str.drain(0..1).collect::<VecDeque<_>>();
+        joined_str.push_str(join_str(&a).as_str());
+        if joined_str == "remaerd" {
+            continue;
+        }
+        println!("NO");
+        return;
+    }
+}
+
+fn join_str(chars: &VecDeque<char>) -> String {
+    chars
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join("")
 }
